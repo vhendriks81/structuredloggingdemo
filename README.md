6c15de0eaf5a21bdbc2839327561b16df478b3c7
Vincent Hendriks (RS) <vincent.hendriks@teamrockstars.nl>
2019-05-19 22:51:17 +0200

    Unused namespaces

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# structuredloggingdemo
+# A demo application to demonstrate structured logging using ASP.NET Core, Serilog and seq
+
--- src/DemoApi/Startup.cs
+++ src/DemoApi/Startup.cs
@@ -1,26 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DemoApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Serilog;
 
 namespace DemoApi
 {
-    using System.Net;
     using DemoApi.Tasks;
     using Destructurama;
     using Hellang.Middleware.ProblemDetails;
-    using Microsoft.AspNetCore.Diagnostics;
-    using Microsoft.AspNetCore.Http;
 
     public class Startup
     {
